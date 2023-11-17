475c833da5475307982adba9ba03e7bd8b5a4c87
shay213 <dawidpawelec@o2.pl>
2023-11-17 14:40:07 +0100

    Add simple animation of chartacter from sprite sheet

--- main.js
+++ main.js
@@ -6,12 +6,25 @@ const CANVAS_HEIGHT = canvas.height = 600
 
 const playerImage = new Image()
 playerImage.src = 'assets/shadow_dog.png'
-let x = 0
+
+const spriteWidth = 575
+const spriteHeight = 523
+const staggerFrames = 5
+
+let frameX = 0
+let frameY = 0
+let gameFrame = 0
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.fillRect(x, 50, 100, 100)
-  x++
+  //ctx.fillRect(50, 50, 100, 100)
+  //ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+  if(gameFrame % staggerFrames === 0){
+    if(frameX < 6) frameX++
+    else frameX = 0
+  }
+  gameFrame++
   requestAnimationFrame(animate)
 }
 
